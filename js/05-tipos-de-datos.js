9751f171acf701e1e4461b9404d9131a1b60c3c0
Gerardo Hernandez <gerardo-hernandez03@outlook.com>
2023-08-31 21:30:14 -0500

    Aprendimos a diferenciar y a identificar los tipos de datos

---
+++ js/05-tipos-de-datos.js
@@ -0,0 +1,27 @@
+'use strict'
+
+//Operadores
+
+/* Los operadores son utilizados para realizar operaciones
+entonces es matematico, cuando se haga variable si quieres
+que sea un numero va sin comillasc ""*/
+
+var numero1 = 7;
+var numero2 = 12;
+var operacion = numero1 * numero2;
+alert("El resultado de la operación es: "+operacion);
+
+//Tipos de datos
+
+var numero_entero = 44;
+var cadena_texto = "Hola que tal";
+var verdadero_o_falso = true;
+var numero_falso = "33.5";
+
+
+//Para saber que tipo de variable tengo se usa lo siguiente
+console.log(typeof numero_entero);
+console.log(typeof cadena_texto);
+console.log(typeof verdadero_o_falso);
+console.log(typeof numero_falso);
+
