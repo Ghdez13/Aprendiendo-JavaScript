8c38e06c52d396969ba3ef476ffce882117dfbd4
Gerardo Hernandez <gerardo-hernandez03@outlook.com>
2023-08-31 20:35:06 -0500

    Terminamos el primer ejercicio de Hola Mundo

--- js/01-hola-mundo.js
+++ js/01-hola-mundo.js
@@ -1,6 +1,6 @@
-/*
-alert("Hola Mundo Con JS");
-alert("Bienvenido al curso");*/
+
+//alert("Hola Mundo Con JS");
+alert("Bienvenido al curso");
 console.log("Muestra esto en la consola");
 console.log(5*5);
 
