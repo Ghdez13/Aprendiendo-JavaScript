ea365c3fb3d5e31d1e1ccda68256b566c718ae86
Gerardo Hernandez <gerardo-hernandez03@outlook.com>
2023-08-31 21:08:45 -0500

    Aprendimos lo que es una constante

---
+++ js/04-constantes.js
@@ -0,0 +1,13 @@
+'use strict'
+
+//Constantes
+/*Una constante es un contener de datos, al igual que una
+variable, pero a diferencia de una variable, no puede ser modificada
+en el transcurso de nuestro programa*/
+
+var web = "https://google.com";
+const ip = "192.89.8.12";
+
+web = "https://victorroblesweb.es";
+
+console.log(web,ip);
