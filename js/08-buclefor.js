caa59cd4b602774c939dec18103902eae08df8a8
Gerardo Hernandez <gerardo-hernandez03@outlook.com>
2023-09-01 21:32:21 -0500

    Aprendimos como usar el bucle for

---
+++ js/08-buclefor.js
@@ -0,0 +1,13 @@
+'use strict'
+
+//Bucle for
+
+/* Un bucle es una estructura de control 
+que se repite varias veces, normalmente es definidio
+por un numero que nostros pongamos */
+
+var numero = 100;
+
+for(var i = 0; i <= numero; i++){
+    console.log("Vamos por el numero: "+i);
+}
