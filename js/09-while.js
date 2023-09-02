ebdb3bf4532bf2a30ba2c8d68ceb184faf665282
Gerardo Hernandez <gerardo-hernandez03@outlook.com>
2023-09-01 21:52:02 -0500

    Aprendimos como utulizar el do whilw y como parar un bucle con un break

--- js/09-while.js
+++ js/09-while.js
@@ -10,5 +10,19 @@ var year = 2018;
 while(year != 1991){
     //Ejecutar esto
     console.log("Estamos en el año: "+ year);
+
+    if(year == 2000){
+        break;
+    }
+
     year--;
 }
+
+// Do while
+
+var years = 30;
+
+do{
+    alert("Solo cuando sesa diferente a 20");
+    years--;
+}while(years > 25)
