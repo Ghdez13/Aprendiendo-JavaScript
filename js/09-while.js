b4e8843a594ffcf61600820ccb545c114d009219
Gerardo Hernandez <gerardo-hernandez03@outlook.com>
2023-09-01 21:44:54 -0500

    Aprendimos como utilizar el bucle while

---
+++ js/09-while.js
@@ -0,0 +1,14 @@
+'use strict'
+
+//Bucle While
+
+/*Este bucle nos sirve para que se haga un bucle
+infinito, para poder salir le tenemos que poner un tope */
+
+var year = 2018;
+
+while(year != 1991){
+    //Ejecutar esto
+    console.log("Estamos en el año: "+ year);
+    year--;
+}
