0e8a209fc70b757a98437560c19855a2a68e0bfd
Gerardo Hernandez <gerardo-hernandez03@outlook.com>
2023-09-15 18:36:23 -0500

    Aprendimos algunos parametros de funciones

--- js/19-ejercicio9.js
+++ js/19-ejercicio9.js
@@ -3,18 +3,39 @@
 // Funciones
 // Una función es una agrupación de un conjunto de instrucciones
 
-function calculadora(){
+function calculadora(numero1, numero2, mostrar=false){
 
     //conjunto de instrucciones a ejecutar
-    console.log("Hola, soy la calculadora");
-    console.log("Si, soy yo"); 
+    if(mostrar == false){
+    console.log("Suma: "+ (numero1+numero2));
+    console.log("Resta: "+ (numero1-numero2));
+    console.log("Multiplicación: "+ (numero1*numero2));
+    console.log("división: "+ (numero1/numero2));
+    console.log("***********************");
+    }else{
+    document.write("Suma: "+ (numero1+numero2) + "<br/>");
+    document.write("Resta: "+ (numero1-numero2) + "<br/>");
+    document.write("Multiplicación: "+ (numero1*numero2) + "<br/>");
+    document.write("división: "+ (numero1/numero2) + "<br/>");
+    document.write("***********************" + "<br/>");
+    }
+    
 
-    //return "Hola soy una calculadora";
+   // return "Hola soy una calculadora";
 }
 
 //Invocar o llamar a la función
-calculadora();
-calculadora();
-calculadora();
-calculadora();
+
+calculadora(1,4);
+calculadora(1,4, true);
+
+// Función con un bucle FOR
+
+/*
+for(var i = 1; i <= 10; i++){
+    console.log(i);
+    calculadora(i, 8);
+}
+*/
+
 
