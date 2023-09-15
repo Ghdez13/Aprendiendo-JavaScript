66c423caf6dbc13836a6fee271ccee8bb8283afa
Gerardo Hernandez <gerardo-hernandez03@outlook.com>
2023-09-15 18:55:00 -0500

    Aprendimos como utilizar los parametros REST Y SPREAD

--- js/19-ejercicio9.js
+++ js/19-ejercicio9.js
@@ -3,23 +3,32 @@
 // Funciones
 // Una función es una agrupación de un conjunto de instrucciones
 
-function calculadora(numero1, numero2, mostrar=false){
-
-    //conjunto de instrucciones a ejecutar
-    if(mostrar == false){
+function porConsola(numero1, numero2){
     console.log("Suma: "+ (numero1+numero2));
     console.log("Resta: "+ (numero1-numero2));
     console.log("Multiplicación: "+ (numero1*numero2));
     console.log("división: "+ (numero1/numero2));
     console.log("***********************");
-    }else{
+}
+
+function porPantalla(numero1, numero2){
     document.write("Suma: "+ (numero1+numero2) + "<br/>");
     document.write("Resta: "+ (numero1-numero2) + "<br/>");
     document.write("Multiplicación: "+ (numero1*numero2) + "<br/>");
     document.write("división: "+ (numero1/numero2) + "<br/>");
     document.write("***********************" + "<br/>");
+}
+
+function calculadora(numero1, numero2, mostrar=false){
+
+    //conjunto de instrucciones a ejecutar
+    if(mostrar == false){
+        porConsola(numero1, numero2);
+    }else{
+        porPantalla(numero1, numero2);
     }
     
+    return true;
 
    // return "Hola soy una calculadora";
 }
@@ -27,7 +36,9 @@ function calculadora(numero1, numero2, mostrar=false){
 //Invocar o llamar a la función
 
 calculadora(1,4);
-calculadora(1,4, true);
+calculadora(2,5, true);
+calculadora(4,5, true);
+calculadora(10,8);
 
 // Función con un bucle FOR
 
