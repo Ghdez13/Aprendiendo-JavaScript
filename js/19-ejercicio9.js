1e353defbfc6ee745767819a6f22b8fdf5ca7d49
Gerardo Hernandez <gerardo-hernandez03@outlook.com>
2023-09-15 18:21:03 -0500

    Cambiamos el ejercicio -19 para iniciar con las funciones

--- js/19-ejercicio9.js
+++ js/19-ejercicio9.js
@@ -1,50 +1,20 @@
 'use strict'
 
-/* Utilizaremos este ultimo ejercicio para poder aprender
-a usar los diferentes operadores
- */
+// Funciones
+// Una función es una agrupación de un conjunto de instrucciones
 
-document.write("<h1> Ejemplos de operadores" + "</h1><br/>")
+function calculadora(){
 
+    //conjunto de instrucciones a ejecutar
+    console.log("Hola, soy la calculadora");
+    console.log("Si, soy yo"); 
 
+    //return "Hola soy una calculadora";
+}
 
-var numero1 = parseInt(prompt("Introduce el primer numéro: ", 0));
-var numero2 = parseInt(prompt("Introduce el segundo numéro: ",0));
-
-document.write("<h2>En este ejemplo estamos sumando" + "</h2>");
-document.write("<p> Al sumar " + numero1 + " + " + numero2 + " obtemos: " + (numero1 + numero2) + "</p>");
-
-document.write("<h2>En este ejemplo estamos resta" + "</h2>");
-document.write("<p> Al restar " + numero1 + " - " + numero2 + " obtemos: " + (numero1 - numero2) + "</p>");
-
-document.write("<h2>En este ejemplo estamos multiplicando" + "</h2>");
-document.write("<p> Al multiplicar " + numero1 + " * " + numero2 + " obtemos: " + (numero1 * numero2) + "</p>");
-
-document.write("<h2>En este ejemplo estamos dividiendo" + "</h2>");
-document.write("<p> Al dividir " + numero1 + " / " + numero2 + " obtemos: " + (numero1 / numero2) + "</p>");
-
-document.write("<h2>En este ejemplo obtenemos el residuo o restante de una división" + "</h2>")
-document.write("<p> Al dividir " + numero1 + " / " + numero2 + " el residuo (%) es: " + (numero1 % numero2) + "</p>");
-
-
-/* Estos de aca abajo son los operadores de asignación., cuando se 
- corre este programa pasa que la variable numero1, toma el valor de la abterior
- por lo que el resultado es otro.
-
-document.write("<h2>En este ejemplo utilizamos '+=' " + "</h2>");
-document.write("<p>Si el primer numero es igual al primero más el segundo, entonces " + numero1 + " =  " + (numero1 += numero2) + "</p>");
-
-document.write("<h2>En este ejemplo utilizamos '-=' " + "</h2>");
-document.write("<p>Si el primer numero es igual al primero menos el segundo, entonces " + numero1 + " =  " + (numero1 -= numero2) + "</p>");
-
-document.write("<h2>En este ejemplo utilizamos '*=' " + "</h2>");
-document.write("<p>Si el primer numero es igual al primero por el segundo, entonces " + numero1 + " =  " + (numero1 *= numero2) + "</p>");
-
-document.write("<h2>En este ejemplo utilizamos '/=' " + "</h2>");
-document.write("<p>Si el primer numero es igual al primero entre el segundo, entonces " + numero1 + " =  " + (numero1 /= numero2) + "</p>");
-
-document.write("<h2>En este ejemplo utilizamos '%=' " + "</h2>");
-document.write("<p>Si el residuo del primer numero es igual al primero entre el segundo, entonces " + numero1 + " =  " + (numero1 %= numero2) + "</p>");
-
-*/
+//Invocar o llamar a la función
+calculadora();
+calculadora();
+calculadora();
+calculadora();
 
