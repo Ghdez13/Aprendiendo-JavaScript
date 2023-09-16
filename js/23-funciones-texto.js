5bbaf081b27c1e0745938caacf92c34ba96fb31d
Gerardo Hernandez <gerardo-hernandez03@outlook.com>
2023-09-15 20:13:21 -0500

    Estamos aprendiendo a como identificar el ambito de las variables

---
+++ js/23-funciones-texto.js
@@ -0,0 +1,12 @@
+'use strict'
+
+//Transformacion de textos de cadenas
+
+var numero = 444;
+var texto1 = "Bienvenido al curso de JavaScript";
+var texto2 = "Es un muy buen curso";
+
+var dato = numero.toString();
+
+console.log(typeof dato);
+
