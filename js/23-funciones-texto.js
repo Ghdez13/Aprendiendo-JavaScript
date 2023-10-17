65d92e14117081feb5db2003e715b09ab7c16298
Gerardo Hernandez <gerardo-hernandez03@outlook.com>
2023-10-16 22:04:56 -0500

    Realizamos y practicamos algunas funciones para realizar busquedas

--- js/23-funciones-texto.js
+++ js/23-funciones-texto.js
@@ -3,23 +3,8 @@
 //Transformacion de textos de cadenas
 
 var numero = 444;
-var texto1 = "Bienvenido al curso de JavaScript";
+var texto1 = "Bienvenido al curso de JavaScript de curso de";
 var texto2 = "Es un muy buen curso";
 
-var dato = numero.toString();
-    dato = texto1.toLowerCase();
-    dato = texto2.toUpperCase();
-
-//console.log(dato);
-
-//Calcular la longitud
-
-var nombre = "Gerardo Hernández";
-
-//console.log(nombre.length);
-
-//Concatenar - Unir un texto
-
-//var textoTotal = texto1+ "" + texto2;
-var textoTotal = texto1.concat(" "+texto2);
-console.log(textoTotal);
+var busqueda = texto1.includes("");
+console.log(busqueda);
