802c82bc731d6471a5c4d530de2844b6c480e890
Gerardo Hernandez <gerardo-hernandez03@outlook.com>
2023-10-16 22:21:59 -0500

    Aprendimos como usar plantillas en JS

--- js/23-funciones-texto.js
+++ js/23-funciones-texto.js
@@ -3,8 +3,9 @@
 //Transformacion de textos de cadenas
 
 var numero = 444;
-var texto1 = "Bienvenido al curso de JavaScript de curso de";
+var texto1 = " Bienvenido al curso de JavaScript de curso  ";
 var texto2 = "Es un muy buen curso";
 
-var busqueda = texto1.includes("");
+var busqueda = texto1.trim();
 console.log(busqueda);
+
