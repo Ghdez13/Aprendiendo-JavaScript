d36f0c64a36e684b1807eb65282aec5690fbc84a
Gerardo Hernandez <gerardo-hernandez03@outlook.com>
2023-10-27 18:49:08 -0500

    Estamos comenzando con pequeños ejercicios de JS, este es del DOM,eventos y funciones

---
+++ js/32-ejercicio-dom.js
@@ -0,0 +1,53 @@
+'use strict'
+
+//Start load
+
+window.addEventListener("load", _ => {
+    console.log("DOM cargado");
+
+    var formulario = document.querySelector("#formulario");
+    var box_dashed = document.querySelector(".dashed");
+    box_dashed.style.display = "none";
+
+    formulario.addEventListener("submit", _ => {
+        console.log("Evento submit caturado");
+        var nombre = document.querySelector("#nombre").value;
+        var apellidos = document.querySelector("#apellidos").value;
+        var edad = document.querySelector("#edad").value;
+
+        box_dashed.style.display = "block";
+
+        console.log(`Nombre: ${nombre} Apellidos: ${apellidos} Edad: ${edad} años`);
+
+        var p_nombre = document.querySelector("#p_nombre span");
+        var p_apellidos = document.querySelector("#p_apellidos span");
+        var p_edad = document.querySelector("#p_edad span");
+        
+        p_nombre.innerHTML = nombre;
+        p_apellidos.innerHTML = apellidos;
+        p_edad.innerHTML = edad;
+        
+        //on a ajouté le suivant 
+        formulario.reset(); 
+
+
+
+        
+
+        /* On va utiliser l'option ici-dessus
+
+        var datos_usuario = [nombre, apellidos, edad];
+        var indice;
+        for (indice in datos_usuario) {
+            var parrafo = document.createElement("p");
+            parrafo.append(datos_usuario[indice]);
+            box_dashed.append(parrafo);
+        } */
+
+
+    });
+
+
+
+
+});//End load
