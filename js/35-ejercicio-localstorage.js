6c5738ec15a747ff804e2419f8fc7b972bce35e9
Gerardo Hernandez <gerardo-hernandez03@outlook.com>
2023-11-04 12:03:09 -0500

    Aprendimos como crear y borrar objetos de JSON

--- js/35-ejercicio-localstorage.js
+++ js/35-ejercicio-localstorage.js
@@ -6,16 +6,16 @@ window.addEventListener("load", _ => {
 
     //On va créer une formulaire de films
 
-    var formulario = document.querySelector("#formpeliculas");
+    var formulario = document.querySelector("#FormPeliculas");
 
     formulario.addEventListener("submit", function () {
-        var titulo = document.querySelector("#addpelicula").value;
+        var titulo = document.querySelector("#AddPelicula").value;
         if(titulo.length >=1){
         localStorage.setItem(titulo, titulo);
         }
     });
 
-    var ul = document.querySelector("#peliculalist");
+    var ul = document.querySelector("#PeliculaList");
     for(var i in localStorage){
         
         if(typeof localStorage[i] == "string"){
@@ -24,7 +24,16 @@ window.addEventListener("load", _ => {
             ul.append(li);
 
         }
-        
     }
+
+    // On va suprimer les films
+    var formulario = document.querySelector("#FormBorrarPeliculas");
+
+    formulario.addEventListener("submit", function () {
+        var titulo = document.querySelector("#BorrarPelicula").value;
+        if(titulo.length >=1){
+        localStorage.removeItem(titulo, titulo);
+        }
+    });
 
 }); //Load Ends
