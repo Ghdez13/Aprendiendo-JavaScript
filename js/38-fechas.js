e9552166fe7b65f0e6028b2642d1990e065665e1
Gerardo Hernandez <gerardo-hernandez03@outlook.com>
2023-11-05 20:45:53 -0500

    En el ejercicio 38, aprendimos a trabajar con fechas y con algunas instrucciones matematicas

---
+++ js/38-fechas.js
@@ -0,0 +1,31 @@
+'use strict'
+
+//Load starts
+window.addEventListener("load", _ =>{
+
+    /*
+    Dans cet exemple on va travailler avec la date, les heures, les jours, etc.
+    */
+
+    var fecha = new Date();
+    var year = fecha.getFullYear();
+    var month = fecha.getMonth();
+    var day = fecha.getDay(); // Cette méthode renvoie le jour de la semaine où (0 = dimanche, 1 = lundi et ainsi de suite)
+    var days = fecha.getDate();
+    var hour = fecha.getHours();
+    var minutes = fecha.getMinutes();
+
+    var fechaCompleta = `
+        El año es: ${year}
+        El mes es: ${month}, donde 0 = enero, 1 = febrero, 2 = marzo ... 11 = diciembre.
+        El día de la semana es: ${day}, donde 0 = domingo, 1 = lunes ... 6 = sabado.
+        El dia del mes es: ${days}
+        La hora es: ${hour}
+        Lis minutos son: ${minutes}
+        `;
+
+    console.log(fechaCompleta);
+    console.log(Math.random()*100); // on multiplie par 100 si nous voulons un nombre à deux chiffres, par 1000 à trois chiffres, etc.
+    console.log(Math.ceil(Math.random()*100)); // On utilse (math.ceil) pour obtenir un nombre sans décimal
+
+});// Load Ends
