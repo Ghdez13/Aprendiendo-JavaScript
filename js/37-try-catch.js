852c7542f14d162d034804e15017fd2c3cb159e9
Gerardo Hernandez <gerardo-hernandez03@outlook.com>
2023-11-05 19:08:13 -0500

    En el ejercicio 37, aprendimos como utilizar el Try-Catch para saber que error cometemos

---
+++ js/37-try-catch.js
@@ -0,0 +1,17 @@
+'use strict'
+
+//Load Starts
+window.addEventListener("load", _=>{
+
+    try{
+        var year = 2019;
+        alert(year);
+        var vector = new Array(99999999999999);
+        console.log(decodeURIComponent("https://google.com"));
+    }catch(error){
+        console.log(error);
+        alert("A ocurrido un error en el codigo");
+    }
+
+
+}); //Load Ends
